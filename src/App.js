034182e360250d1b452a2e77f1ed7908f6e782da
byionline <10815566+byionline@users.noreply.github.com>
2020-10-22 23:21:34 +0530

    Added 2 more images in slider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import './styles.css';
 import makeup from "./assets/img/makeup.jpg";
+import juice from "./assets/img/juice.jpg";
+import moisture from "./assets/img/moisture.jpg";
 import './styles.css';
 function App() {
   const slides = [];
@@ -12,6 +14,24 @@ function App() {
         src={makeup}
         style={{ listStyle: 'none' }}
         alt="Slide 0"
+      />
+    </SwiperSlide>
+  )
+  slides.push(
+    <SwiperSlide key="slide-1">
+      <img
+        src={juice}
+        style={{ listStyle: 'none' }}
+        alt="Slide 1"
+      />
+    </SwiperSlide>
+  )
+  slides.push(
+    <SwiperSlide key="slide-2">
+      <img
+        src={moisture}
+        style={{ listStyle: 'none' }}
+        alt="Slide 2"
       />
     </SwiperSlide>
   )
