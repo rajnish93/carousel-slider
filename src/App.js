5b1e79f884f4e60b58bc733be091cd8bbc270946
byionline <10815566+byionline@users.noreply.github.com>
2020-10-22 23:18:51 +0530

    Added one slide image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import './styles.css';
+import makeup from "./assets/img/makeup.jpg";
 import './styles.css';
 function App() {
-  return <div className="App">Hello World!</div>
+  const slides = [];
+  slides.push(
+    <SwiperSlide key="slide-0">
+      <img
+        src={makeup}
+        style={{ listStyle: 'none' }}
+        alt="Slide 0"
+      />
+    </SwiperSlide>
+  )
+  return <React.Fragment>
+    <Swiper id="main">
+      {slides}
+    </Swiper>
+  </React.Fragment>
 }
 export default App;
